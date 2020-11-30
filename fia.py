31bdc6ad9bf9f64beb546ee4d23fbc1bfd315205
badgley <badgley@gbadgley.com>
2020-11-30 00:56:24 -0500

    more bugs -- closer

--- fia.py
+++ fia.py
@@ -14,7 +14,7 @@ def load_arb_shapes(postal_code):
     if postal_code == 'ak':
         gdf = gpd.read_file('/home/jovyan/carbonplan/forests/notebooks/ak_assessment_areas/')
     else:
-        gdf = pd.read_file('/home/jovyan/carbonplan/forests/notebooks/2015_arb_supersections/')
+        gdf = gpd.read_file('/home/jovyan/carbonplan/forests/notebooks/2015_arb_supersections/')
     return gdf.to_crs('epsg:4326')
 
 
@@ -37,11 +37,8 @@ def load_ecomap(postal_code):
     return ecomap.to_crs('epsg:4326')
 
 
-
 def load_fia_data(postal_code, kind=None, filter_data=True):
-    """
-
-    """
+    """"""
     if kind not in ['long', 'tree']:
         raise NotImplementedError('kind must be in ["long", "tree"]')
 
@@ -53,11 +50,12 @@ def load_fia_data(postal_code, kind=None, filter_data=True):
 
     if filter_data:
         if postal_code == 'ak':
-            criteria = (df['INVYR'] > 2000) & (df['INVYR'] < 2013) & (~df['OWNCD'].isin([21, 31, 32]))
+            criteria = (
+                (df['INVYR'] > 2000) & (df['INVYR'] < 2013) & (~df['OWNCD'].isin([21, 31, 32]))
+            )
         else:
             criteria = (df['INVYR'] > 2000) & (df['INVYR'] < 2013) & (df['OWNCD'] == 46)
         df = df[criteria]
-
 
     # assign all regions we might be interested in!
     # this is slower for tree bc we assign EACH tree as opposed to each plot...but yolo.
@@ -75,6 +73,7 @@ def load_fia_data(postal_code, kind=None, filter_data=True):
         )
 
     return df
+
 
 def load_fia_long(postal_code):
 
@@ -112,8 +111,6 @@ def load_fia_long(postal_code):
     return df
 
 
-
-
 def load_fia_tree(postal_code):
     cond_df = pd.read_parquet(
         f'gs://carbonplan-data/raw/fia-states/cond_{postal_code.lower()}.parquet',
@@ -125,7 +122,6 @@ def load_fia_tree(postal_code):
         f'gs://carbonplan-data/raw/fia-states/plot_{postal_code}.parquet',
         columns=['CN', 'LAT', 'LON', 'ELEV', 'INVYR'],
     )
-
 
     tree_df = pd.read_parquet(
         f'gs://carbonplan-data/raw/fia-states/tree_{postal_code.lower()}.parquet',
