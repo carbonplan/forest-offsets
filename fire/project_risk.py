9cb42b155427120c4c04a2d8a6b92d5f5f2b298b
Joseph Hamman <jhamman@ucar.edu>
2020-12-16 13:27:19 -0800

    package infrastructure bits

--- fire/project_risk.py
+++ fire/project_risk.py
@@ -1,25 +1,30 @@
 import os
-import fire
+
 import fsspec
-import xarray as xr
 import geopandas as gp
 import regionmask as rm
+import xarray as xr
 from shapely.geometry import Point
+
+import fire
+
 
 def integrated_risk(p):
     from scipy.stats import binom
+
     return (1 - binom.cdf(0, 100, p)) * 100
+
 
 def average_risk(da, mask):
     return (
-        da
-        .where(mask)
+        da.where(mask)
         .mean(['x', 'y'])
         .groupby('time.year')
         .sum()
-        .sel(year=slice('2001','2018'))
+        .sel(year=slice('2001', '2018'))
         .mean()
     )
+
 
 def query_by_ecoregion(da, lon, lat, regions):
     p = Point(lon, lat)
@@ -27,16 +32,19 @@ def query_by_ecoregion(da, lon, lat, regions):
     ecoregion = [i for i in regions.index if regions.geometry[i].contains(p)][0]
     return average_risk(da, masks[ecoregion])
 
+
 def query_by_shape(da, shape):
     mask = rm.mask_3D_geopandas(shape.simplify(0.002).buffer(0.005), da)[0]
     return average_risk(da, mask)
 
+
 def query_by_location(da, lon, lat):
     latgrid = da['lat']
     longrid = da['lon']
-    dist = (longrid - lon) **2 + (latgrid - lat) **2
+    dist = (longrid - lon) ** 2 + (latgrid - lat) ** 2
     mask = dist < 0.5
     return average_risk(da, mask)
+
 
 def project_risk(store='local', mode='ecoregion', lat=None, lon=None, id=None):
     if store == 'local':
@@ -55,12 +63,13 @@ def project_risk(store='local', mode='ecoregion', lat=None, lon=None, id=None):
 
     if mode == 'shape':
         shape = gp.read_file(prefix + f'/raw/projects/{id}.json')
-        risk = integrated_risk(query_by_shape(da, shape)) 
+        risk = integrated_risk(query_by_shape(da, shape))
         print(risk)
 
     if mode == 'location':
-        risk = integrated_risk(query_by_location(da, lon, lat)) 
+        risk = integrated_risk(query_by_location(da, lon, lat))
         print(risk)
 
+
 if __name__ == '__main__':
-  fire.Fire(project_risk)
+    fire.Fire(project_risk)
