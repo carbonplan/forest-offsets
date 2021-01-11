07ddd5905bbc4138ae81bfb889ddf781a04a5a1f
badgley <graysonbadgley@gmail.com>
2021-01-11 17:42:50 -0500

    added function to scale ifm1 to arbocs

--- carbonplan_retro/analysis/common_practice.py
+++ carbonplan_retro/analysis/common_practice.py
@@ -85,3 +85,51 @@ def supersection_subset(
     data = data[data['supersection_id'] == supersection_id]
 
     return data
+
+
+def get_arbocs_from_ifm1(new_ifm1, project_db, name='new_allocation'):
+    """recalcualte arbocs as a function of predicted ifm1
+    only ifm1 changes -- all other components are scaled to be proportional to observed <component>/ifm_1
+    """
+
+    # ratio terms
+    ratio_ifm_3_ifm_1 = (
+        project_db['baseline']['components']['ifm_3']
+        / project_db['baseline']['components']['ifm_1']
+    )
+
+    ratio_woodproducts_ifm_1 = (
+        project_db['baseline']['components']['ifm_7']
+        + project_db['baseline']['components']['ifm_8']
+    ) / (project_db['baseline']['components']['ifm_1'])
+
+    ratio_secondary_effects_ifm_1 = project_db['rp_1']['secondary_effects'] / (
+        project_db['baseline']['components']['ifm_1']
+    )
+
+    # ifm-1 + ifm-3
+    alt_baseline_carbon = new_ifm1 + (new_ifm1 * ratio_ifm_3_ifm_1)
+
+    onsite_carbon = (
+        project_db['rp_1']['components']['ifm_1'] + project_db['rp_1']['components']['ifm_3']
+    )
+
+    adjusted_onsite = onsite_carbon * (1 - project_db['rp_1']['confidence_deduction']).astype(
+        float
+    ).round(5)
+
+    delta_onsite = adjusted_onsite - alt_baseline_carbon
+
+    baseline_wood_products = new_ifm1 * ratio_woodproducts_ifm_1
+
+    actual_wood_products = (
+        project_db['rp_1']['components']['ifm_7'] + project_db['rp_1']['components']['ifm_8']
+    )
+
+    leakage_adjusted_delta_wood_products = (actual_wood_products - baseline_wood_products) * 0.8
+
+    secondary_effects = new_ifm1 * ratio_secondary_effects_ifm_1
+    secondary_effects[secondary_effects > 0] = 0  # Never allowed to have positive SE.
+
+    calculated_allocation = delta_onsite + leakage_adjusted_delta_wood_products + secondary_effects
+    return calculated_allocation.rename(name)
