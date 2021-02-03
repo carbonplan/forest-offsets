68516456c59a4340927eb8dc9753d9f700698a0e
badgley <graysonbadgley@gmail.com>
2021-02-03 13:28:02 -0500

    reverted rfia json script back to assessment area specific -- rFIA bonks when load entire supersection

--- scripts/preprocess/write_rfia_json.py
+++ scripts/preprocess/write_rfia_json.py
@@ -48,28 +48,37 @@ if __name__ == '__main__':
     aa_ss_dict = aa_code_to_ss_code()
 
     if args.all:
-        supersection_list = list(range(1, 98))
+        project_list = list(range(1, 98))
         out_fn = 'rfia_assessment_areas_all.json'
     else:
-        supersection_list = PROJECT_SUPERSECTIONS
+        project_list = PROJECT_SUPERSECTIONS
         out_fn = 'rfia_assessment_areas_subset.json'
 
     store = []
-    for supersection_id in supersection_list:
+    for aa_id, fortypcds in arb_fortyps.items():
+        ss_id = aa_ss_dict.get(aa_id)
 
-        supersections = load_supersections(include_ak=False, fix_typos=True)
+        if ss_id == 62:
+            # skipping Ouachita Mixed Forest, because it doesnt have a shape!
+            continue
 
-        supersection = supersections.loc[supersections['ss_id'] == supersection_id]
-        record = {
-            'supersection_name': supersection['SSection'].item(),
-            'supersection_id': supersection['ss_id'].item(),
-            'postal_codes': [
-                postal_code.upper()
-                for postal_code in get_overlapping_states(supersection.geometry.item())
-            ],
-        }
+        if ss_id in project_list:
 
-        store.append(record)
+            supersections = load_supersections(include_ak=False, fix_typos=True)
+
+            supersection = supersections.loc[supersections['ss_id'] == ss_id]
+            record = {
+                'assessment_area_id': aa_id,
+                'fortypcds': fortypcds,
+                'supersection_name': supersection['SSection'].item(),
+                'supersection_id': supersection['ss_id'].item(),
+                'postal_codes': [
+                    postal_code.upper()
+                    for postal_code in get_overlapping_states(supersection.geometry.item())
+                ],
+            }
+
+            store.append(record)
 
     fn = os.path.join('/home/jovyan/lost+found/', out_fn)
     with fsspec.open(fn, mode='w') as f:
