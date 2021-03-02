9aa975f6e42e749dbb097f05071c4f018c889487
badgley <graysonbadgley@gmail.com>
2021-03-02 22:56:11 +0000

    added script to push results of rFIA from server to blobstorage

---
+++ scripts/preprocess/push_rfia_to_azure.py
@@ -0,0 +1,14 @@
+import os
+
+import fsspec
+
+if __name__ == '__main__':
+    fs = fsspec.get_filesystem_class('az')(
+        account_name='carbonplan', account_key=os.environ['BLOB_ACCOUNT_KEY']
+    )
+    fs.put(
+        '/home/jovyan/rfia/processed_data/no_buffer_biomass/',
+        'carbonplan-retro/rfia',
+        recursive=True,
+        overwrite=True,
+    )
