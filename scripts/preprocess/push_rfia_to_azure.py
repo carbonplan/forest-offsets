7c1f5b34da19616ada5219d1e18f7c0b33455a99
badgley <graysonbadgley@gmail.com>
2021-04-12 20:31:17 +0000

    cleaned up notebooks and repo for release

--- scripts/preprocess/push_rfia_to_azure.py
+++ scripts/preprocess/push_rfia_to_azure.py
@@ -1,4 +1,4 @@
-from carbonplan_retro.data import get_filesystem
+from carbonplan_forest_offsets.data import get_filesystem
 
 if __name__ == '__main__':
     '''syncs rFIA run outputs to `cat`'''
@@ -13,5 +13,10 @@ if __name__ == '__main__':
         '/home/jovyan/rfia/processed_data/no_buffer_biomass_all/',
         'carbonplan-retro/rfia_all',
         recursive=True,
+        overwrite=True,
+    )
+    fs.put(
+        '/home/jovyan/rfia/processed_data/297_by_ecosection.csv',
+        'carbonplan-retro/results/297_by_ecosection.csv',
         overwrite=True,
     )
