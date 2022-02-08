0a056ea55ffdd7596b52af632a47a06d85b26ce6
badgley <grayson@carbonplan.org>
2022-02-08 18:19:01 -0500

    Create workflow to download and save project geometries
    Still doing a bit of work to sort out targets/caching. Topojson seems to
    be working really well too.

--- carbonplan_forest_offsets/workflows/carb_geometries.py
+++ carbonplan_forest_offsets/workflows/carb_geometries.py
@@ -1,16 +1,26 @@
+import datetime
+import json
+
 import geopandas
 import prefect
 import requests
 import topojson
 
+from carbonplan_forest_offsets.load.issuance import load_issuance_table
+
 CARB_GIS_ENDPOINT = (
     "https://gis.carb.arb.ca.gov/hosting/rest/services/ARBOC_Issuance_Map/MapServer/0/query"
 )
+
+from prefect.engine.results import LocalResult
+
+serializer = prefect.engine.serializers.JSONSerializer()
 
 
 # TODO: Merge 'get most recent' into load
 # TODO: from issuance get arb_id_opr_id map
 # once we have that, template outputs
+
 
 @prefect.task
 def get_object_ids() -> list:
@@ -20,8 +30,17 @@ def get_object_ids() -> list:
     return r.json()['objectIds']
 
 
+@prefect.task(cache_for=datetime.timedelta(hours=1))
+def get_arb_id_to_opr_id_map():
+    df = load_issuance_table(forest_only=False, most_recent=True)[['opr_id', 'arb_id']].set_index(
+        'arb_id'
+    )
+    return df.opr_id.to_dict()
+
+
+
 @prefect.task
-def get_project_geometry(object_id: int) -> geopandas.GeoDataFrame:
+def get_project_geometry(object_id, name_map) -> geopandas.GeoDataFrame:
 
     params = {
         "objectIds": f"{object_id}",
@@ -31,19 +50,33 @@ def get_project_geometry(object_id: int) -> geopandas.GeoDataFrame:
     }
     r = requests.get(CARB_GIS_ENDPOINT, params=params)
     gdf = geopandas.GeoDataFrame.from_features(r.json())
+
+    arb_id = gdf.get('arb_id').item()
+    gdf['opr_id'] = name_map.get(arb_id)
     return gdf
 
-def generate_task_run_name(blah):
-    pass
+def generate_target_name(project_geometry: geopandas.GeoDataFrame, **kwargs):
+    opr_id = project_geometry.get('opr_id').item()
+    return f"{opr_id}.json"
 
-@prefect.task(result=LocalResult(), target="")
+@prefect.task(target=generate_target_name,
+              result=LocalResult(dir='/tmp/raw', serializer=serializer),
+              checkpoint=True)
+def cache_project_geometry(project_geometry: geopandas.GeoDataFrame) -> str:
+    return json.loads(project_geometry.to_json()) # to_dict errors so just convert to_json string and back
+
+@prefect.task(target=generate_target_name,
+              result=LocalResult(dir='/tmp/topo', serializer=serializer),
+              checkpoint=True)
 def get_simplified_project_geometry(project_geometry: geopandas.GeoDataFrame) -> str:
     topo = topojson.Topology(project_geometry)
     simplified = topo.toposimplify(0.001, prevent_oversimplify=True)
-    return simplified.to_json()
+    return json.loads(simplified.to_json()) # to_dict errors so just convert to_json string and back
 
 
 with prefect.Flow(name="carb-geometries") as flow:
+    name_map = get_arb_id_to_opr_id_map()
     object_ids = get_object_ids()
-    geometries = get_project_geometry.map(object_ids)
+    geometries = get_project_geometry.map(object_id=object_ids, name_map=prefect.unmapped(name_map))
+    cache_project_geometry.map(geometries)
     simplified = get_simplified_project_geometry.map(geometries)
